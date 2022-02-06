77bf4b901026d95bd74d95a093dd6ec7eb486502
kokivasileski <thekoki001@gmail.com>
2022-02-06 10:20:58 -0500

    added dark mode to index page

--- indexJS.js
+++ indexJS.js
@@ -3,8 +3,19 @@
 
 const modeButton = document.querySelector(".viewModeButton");
 const bodyDark = document.querySelector("body")
+const indexGallery = document.querySelector(".gallery")
+const indexAbout = document.querySelector(".aboutContainer")
+const indexHeader = document.querySelector(".mainHeader")
+const indexLocations = document.querySelector("locations")
 
 modeButton.addEventListener("click", function() {
+    indexHeader.classList.toggle("indexHeaderDarkMode")
     bodyDark.classList.toggle("darkModeGeneral")
+    indexGallery.classList.toggle("galleryDarkMode")
+    indexAbout.classList.toggle("indexAboutDarkMode")
+
+    const indexImages = document.querySelectorAll("IMG")
+    for (let i = 0; i < indexImages.length; i++)
+        indexImages[i].classList.toggle("indexImageDarkMode")
 });
 
